f892d9c1df5ec7f38020fab25afad7cafaacfba1
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-02-15 19:33:25 -0800

    Implemented day selected state

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,6 +21,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,8 +35,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
