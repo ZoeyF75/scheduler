1dc9b85da84a918caf34f77a2d381070a1b0355a
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-02-15 18:03:52 -0800

    Tests run for DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,20 +2,29 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const spotsAvailable = (spots) => {
+  if (!spots) {
+    return "no spots remaining";
+  } 
+  if (spots === 1) {
+    return "1 spot remaining";
+  } 
+  else {
+    return spots + " spots remaining";
+  }
+}
+
 export default function DayListItem(props) {
-  const dayListItemClass = classNames ("day-list__item", {
-    "day-list__item" : true,
+  const dayListItemClass = classNames("day-list__item", {
     "day-list__item--selected" : props.selected,
     "day-list__item--full" : !(props.spots)
   });
-  
+
   return (
-    <li>
-      className= { dayListItemClass }
-      onClick{() => props.setDay(props.name)}
+    <li className= { dayListItemClass } onClick={() => props.setDay(props.name)} >
       <h2 className="text--regular">{ props.selected }</h2> 
-      <h3 className="text--light">{ props.spots}</h3>
-      { props.children }
+      <h3 className="text--light">{ spotsAvailable(props.spots) }</h3>
     </li>
   );
 }
+
