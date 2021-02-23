36acfb4edc7171038127ce24d5e4dbbed69d27f7
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-02-23 12:08:37 -0800

    Mock axios get requests for days, appointments, interviewers

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,16 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
+import axios from 'axios';
 import Application from "components/Application";
+import { render, cleanup, waitForElement, fireEvent, toBeInTheDocument } from "@testing-library/react";
+
 
 afterEach(cleanup);
+describe("Application", () => {
+  it("defaults to Monday and changes the scedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"))
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+})
 
-it("renders without crashing", () => {
-  render(<Application />);
-});
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -26,9 +26,9 @@ export default function useApplicationData(initial) {
   
   useEffect(() => {
     Promise.all([
-      Promise.resolve(axios.get('http://localhost:8001/api/days')),
-      Promise.resolve(axios.get('http://localhost:8001/api/appointments')),
-      Promise.resolve(axios.get('http://localhost:8001/api/interviewers'))
+      Promise.resolve(axios.get('/api/days')),
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers'))
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })
